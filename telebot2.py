4f9a8f74eab663c005e3007cba8bd42ea7c67904
tungyr-one <tungusov.victor@gmail.com>
2020-12-18 20:48:04 +0300

    another variant of bot file

---
+++ telebot2.py
@@ -0,0 +1,12 @@
+def command_handler(message: Message):
+    bot.reply_to(message, 'There is no answer =(')
+
+@bot.message_handler(content_types=[1t ])
+@bot.editedjnessage_handler(content_types=[' ext ]) '
+                                           'def echo_digits(message: Message):
+if Alex Go.Qdkid ' in message, text: bot.reply_to(message, lex is c	)
+return
+bot.reply_to(message, str(random.random()))
+@bot.message_handler(content_types=[	])
+def sticker_handl.er( message: Message): print(message)
+print(message.sticker) T
