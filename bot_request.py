34ee4c8947e8a48b2e2131eb141400a9f0b2ac86
tungyr-one <tungusov.victor@gmail.com>
2020-12-22 20:37:34 +0300

    bot telebot

---
+++ bot_request.py
@@ -0,0 +1,20 @@
+# https://www.youtube.com/watch?v=LJdu68ro-rU&ab_channel=GeekCode
+
+import requests
+
+from config import TG_TOKEN
+
+MAIN_URL = f'https://api.telegram.org/bot{TG_TOKEN}'
+
+payload = {
+    'chat_id': 354310062,
+    'text': 'Hellow to you!',
+    'reply_to_message_id': 68
+}
+
+# r = requests.get(f'{MAIN_URL}/getUpdates')  # получение сообщения
+r = requests.post(f'{MAIN_URL}/sendMessage', data=payload)  # отсылка сообщения
+print(r)
+
+print(r.json())
+
