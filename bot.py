f6b8001e3c9822ad761b8c29c232eb33c5c46e56
Victor Tungusov <tungusov.victor@gmail.com>
2021-02-16 15:22:41 +0300

    next step, bad lector

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@
 # https://core.telegram.org/bots/api
 
 import random
+import pickle
+
 import telebot
+from telebot import types
 from telebot.types import Message
 
 from config import TG_TOKEN
@@ -11,6 +14,10 @@ from config import TG_TOKEN
 bot = telebot.TeleBot(TG_TOKEN)
 sticker_id = 'CAADAgADOQADfyesDlKEqOOd72VKAg'
 sticker_id2 = 'CAADAgADqAIAAkcVaAllGKpjZDCAgQI'
+
+USERS = set()
+
+
 
 @bot.message_handler(commands=['start', 'help'])
 def command_handler(message: Message):
@@ -22,44 +29,61 @@ def command_handler(message: Message):
         return
 
 
-@bot.message_handler(content_types=['text', "sticker"])  # react to stickers (not working)
-def sticker_handler(message: Message):
-    print(message.chat.id)
-    print(message.content_type)
-    # if message.content_type == ['sticker']:
-    #     bot.send_sticker(message.chat.id, sticker_id2, reply_to_message_id=message.id, )
-    # bot.get_sticker_set('sticker-set')
-    bot.send_sticker(message.chat.id,'https://telegram.org/file/811140823/2/gEXq-ZISs9g/55f6c9c9f87c4dceb0')
-    return
+# @bot.infinity_polling(lambda query: query.query)
+# def query_text(inline_query):
+#     try:
+#         r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+#         r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+#     except Exception as e:
+#         print(e)
 
 
 
-# @bot.message_handler(content_types=['text'])  # бот реагирует на текст
-# @bot.edited_message_handler(content_types=['text'])  # бот реагирует на отредактированный текст тоже
-# def echo_digits(message: Message):
-#     if 'Victor' in message.text:  # реагирование на слово в тексте сообщения
-#         bot.reply_to(message, 'Victor the best!')
-#         return
-#
-#     if 'pow' in message.text:
-#         digit = ''
-#         for i in message.text:
-#             if i.isdigit():
-#                 digit = digit + i
-#         if len(digit) == 0:
-#             digit = 0
-#         result = int(digit) ** int(digit)
-#         bot.reply_to(message, f'The pow of {digit} is {str(result)}')
-#
-#     # if 'pic' in message.text:  # Not working
-#     #     photo = open('girl.jpg', 'rb')
-#     #     bot.reply_to(message, photo)
-#
-#     else:
-#         # bot.reply_to(message, str(random.random()))  # reply as random digit
-#         # bot.reply_to(message, 'Your message: ' + message.text)  # send back user's message text
-#         # print(message.json)
-#         pass
+# @bot.message_handler(content_types=['text', "sticker"])  # react to stickers (not working - react to everything)
+# def sticker_handler(message: Message):
+#     print(message.chat.id)
+#     print(message.content_type)
+#     # if message.content_type == ['sticker']:
+#     #     bot.send_sticker(message.chat.id, sticker_id2, reply_to_message_id=message.id, )
+#     # bot.send_sticker(message.chat.id,'https://telegram.org/file/811140823/2/gEXq-ZISs9g/55f6c9c9f87c4dceb0')
+#     return
+
+
+
+@bot.message_handler(content_types=['text'])  # бот реагирует на текст
+@bot.edited_message_handler(content_types=['text'])  # бот реагирует на отредактированный текст тоже
+def echo_digits(message: Message):
+    print(message.from_user.id)
+    reply = str(random.random())
+    if message.from_user.id in USERS:
+        reply += f'{message.from_user.id}, hello again'
+    bot.reply_to(message, reply)
+    USERS.add(message.from_user.first_name)
+    print(USERS)
+
+    if 'Victor' in message.text:  # реагирование на слово в тексте сообщения
+        bot.reply_to(message, 'Victor the best!')
+        return
+
+    if 'pow' in message.text:
+        digit = ''
+        for i in message.text:
+            if i.isdigit():
+                digit = digit + i
+        if len(digit) == 0:
+            digit = 0
+        result = int(digit) ** int(digit)
+        bot.reply_to(message, f'The pow of {digit} is {str(result)}')
+
+    # if 'pic' in message.text:  # Not working
+    #     photo = open('girl.jpg', 'rb')
+    #     bot.reply_to(message, photo)
+
+    else:
+        # bot.reply_to(message, str(random.random()))  # reply as random digit
+        # bot.reply_to(message, 'Your message: ' + message.text)  # send back user's message text
+        # print(message.json)
+        pass
 #
 # @bot.message_handler(func=lambda m: True)  # ответ на любые сообщения текстом отправленного сообщения
 # def echo_all(message):
