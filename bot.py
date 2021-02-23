e45ff132d098ae68cd0d2d2fc2af3f8266fe0524
Victor Tungusov <tungusov.victor@gmail.com>
2021-02-23 15:00:02 +0300

    test app first attempts

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # doc: https://github.com/eternnoir/pyTelegramBotAPI
 # https://core.telegram.org/bots/api
 
+
 import random
 import pickle
 
@@ -16,17 +17,31 @@ sticker_id = 'CAADAgADOQADfyesDlKEqOOd72VKAg'
 sticker_id2 = 'CAADAgADqAIAAkcVaAllGKpjZDCAgQI'
 
 USERS = set()
+questions = []
 
-
-
+# https://pythonru.com/primery/telegram-bot-na-python-ustanovka-i-nastrojka
+#
 @bot.message_handler(commands=['start', 'help'])
 def command_handler(message: Message):
     if '/help' in message.text:
         bot.reply_to(message, 'This bot is developing by Tungyr')
-    else:
-        bot.reply_to(message, 'Menu is developing now:(')
-        print(message.text)
-        return
+
+    if '/start' in message.text:
+        bot.reply_to(message, 'Test is starting!')
+        quest = test()
+        bot.send_message(message.chat.id, quest)
+        answer_handler(message)
+
+
+    return
+
+
+def test():
+    with open('test.txt', 'r') as f:
+        [questions.append(question) for question in f.readlines()]
+    print(questions)
+    for i in questions:
+        yield i
 
 
 # @bot.infinity_polling(lambda query: query.query)
@@ -39,51 +54,52 @@ def command_handler(message: Message):
 
 
 
-# @bot.message_handler(content_types=['text', "sticker"])  # react to stickers (not working - react to everything)
-# def sticker_handler(message: Message):
-#     print(message.chat.id)
-#     print(message.content_type)
-#     # if message.content_type == ['sticker']:
-#     #     bot.send_sticker(message.chat.id, sticker_id2, reply_to_message_id=message.id, )
-#     # bot.send_sticker(message.chat.id,'https://telegram.org/file/811140823/2/gEXq-ZISs9g/55f6c9c9f87c4dceb0')
-#     return
+@bot.message_handler(content_types=['text'])
+def answer_handler(message: Message):
+    print(message.chat.id)
+    print(message.content_type)
+    bot.reply_to(message, 'Right!')
+    return
 
 
 
-@bot.message_handler(content_types=['text'])  # бот реагирует на текст
-@bot.edited_message_handler(content_types=['text'])  # бот реагирует на отредактированный текст тоже
-def echo_digits(message: Message):
-    print(message.from_user.id)
-    reply = str(random.random())
-    if message.from_user.id in USERS:
-        reply += f'{message.from_user.id}, hello again'
-    bot.reply_to(message, reply)
-    USERS.add(message.from_user.first_name)
-    print(USERS)
-
-    if 'Victor' in message.text:  # реагирование на слово в тексте сообщения
-        bot.reply_to(message, 'Victor the best!')
-        return
-
-    if 'pow' in message.text:
-        digit = ''
-        for i in message.text:
-            if i.isdigit():
-                digit = digit + i
-        if len(digit) == 0:
-            digit = 0
-        result = int(digit) ** int(digit)
-        bot.reply_to(message, f'The pow of {digit} is {str(result)}')
-
-    # if 'pic' in message.text:  # Not working
-    #     photo = open('girl.jpg', 'rb')
-    #     bot.reply_to(message, photo)
-
-    else:
-        # bot.reply_to(message, str(random.random()))  # reply as random digit
-        # bot.reply_to(message, 'Your message: ' + message.text)  # send back user's message text
-        # print(message.json)
-        pass
+# @bot.message_handler(content_types=['text'])  # бот реагирует на текст
+# @bot.edited_message_handler(content_types=['text'])  # бот реагирует на отредактированный текст тоже
+# def echo_digits(message: Message):
+#     print(message.from_user.id)
+#     print(USERS)
+#     if message.from_user.id in USERS:
+#         reply = f'{message.from_user.first_name}, hello again!'
+#     else:
+#         reply = f'Hello {message.from_user.first_name}!'
+#
+#     bot.reply_to(message, reply)
+#     USERS.add(message.from_user.first_name)
+#     print(USERS)
+#
+#     if 'Victor' in message.text:  # реагирование на слово в тексте сообщения
+#         bot.reply_to(message, 'Victor the best!')
+#         return
+#
+#     if 'pow' in message.text:
+#         digit = ''
+#         for i in message.text:
+#             if i.isdigit():
+#                 digit = digit + i
+#         if len(digit) == 0:
+#             digit = 0
+#         result = int(digit) ** int(digit)
+#         bot.reply_to(message, f'The pow of {digit} is {str(result)}')
+#
+#     # if 'pic' in message.text:  # Not working
+#     #     photo = open('girl.jpg', 'rb')
+#     #     bot.reply_to(message, photo)
+#
+#     else:
+#         # bot.reply_to(message, str(random.random()))  # reply as random digit
+#         # bot.reply_to(message, 'Your message: ' + message.text)  # send back user's message text
+#         # print(message.json)
+#         pass
 #
 # @bot.message_handler(func=lambda m: True)  # ответ на любые сообщения текстом отправленного сообщения
 # def echo_all(message):
@@ -95,4 +111,6 @@ def echo_digits(message: Message):
 # upd = bot.get_updates()  # получение обновлений, как вытащить из него данные?
 # for i in upd:
 #     print(i)
+
+
 bot.polling(timeout=60)
